a3b91d53d4dfd50e2cfeae2049739f051a892e01
Nico Wendell <nicowendell@gmail.com>
2016-06-21 21:04:52 -0700

    Initial block interface

---
+++ src/ffs/ffs_block.h
@@ -0,0 +1,31 @@
+#define FFS_BLOCK_H
+#ifndef FFS_BLOCK_H
+
+#define FFS_BLOCK_FREE		 0
+#define FFS_BLOCK_LAST		-2
+#define FFS_BLOCK_INVALID	-1
+
+// Allocate a new block after specified block
+// If specified block is FFS_BLOCK_LAST then newly allocated block is first
+// Returns new block on success; otherwise, returns FFS_BLOCK_INVALID
+int ffs_block_alloc(ffs_disk disk, int previous_block);
+
+// Mark all blocks in the block list starting at specified block as FFS_BLOCK_FREE
+// Returns zero on success; otherwise, returns non-zero
+int ffs_block_free(ffs_disk disk, int block);
+
+// Get the next block in the block list
+// Returns next block on success; otherwise, returns FFS_BLOCK_LAST
+int ffs_block_next(ffs_disk disk, int block);
+
+// Read entire contents of specified block to buffer
+// Buffer must be size of a block
+// Returns zero on success; otherwise, returns non-zero
+int ffs_block_read(ffs_disk disk, int block, void *buffer);
+
+// Write entire contents of specified block from buffer
+// Buffer must be size of a block
+// Returns zero on success; otherwise, returns non-zero
+int ffs_block_write(ffs_disk disk, int block, const void *buffer);
+
+#endif
